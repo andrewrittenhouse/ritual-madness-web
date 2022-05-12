03f5a5a97cdc71b29c73a6f2b1a214eabf189fb9
Andrew Rittenhouse <andrewrittenhouse@gmail.com>
2022-05-12 18:50:53 -0400

    cool input

--- public/magic-shape.js
+++ public/magic-shape.js
@@ -2,29 +2,42 @@ function calculateAngle(index, pointCount) {
     return (((index + 1) / pointCount) * 2 * Math.PI) + Math.PI;
 }
 
-
-function drawMagicShape(canvas, ctx, { pointCount, radius, beamProbability, beamColor }) {
+function calculatePoints({ pointCount, radius, xOffset, yOffset }) {
     const points = [];
 
     for (let i = 0; i < pointCount; i++) {
         const angle = calculateAngle(i, pointCount);
 
-        const x = Math.sin(angle) * radius + (canvas.width / 2);
-        const y = Math.cos(angle) * radius + (canvas.height / 2);
+        const x = Math.sin(angle) * radius + xOffset;
+        const y = Math.cos(angle) * radius + yOffset;
 
         points.push([x, y]);
     }
 
+    return points;
+}
+
+function drawMagicShape(canvas, ctx, { asymmetry, points, beamProbability, beamColor }) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.lineWidth = 1;
-
     ctx.strokeStyle = 'black';
 
     ctx.beginPath();
 
-    for (let i = 0; i < pointCount; i++) {
-        for (let ii = i + 1; ii < pointCount; ii++) {
+    const missingPointCount = Math.round(points.length * asymmetry);
+
+    const startIndex = missingPointCount;
+    const endIndex = points.length - missingPointCount;
+
+    const disconnectedPointCount = missingPointCount * 2;
+
+    for (let i = startIndex; i < endIndex; i++) {
+        for (let ii = i + 1; ii < endIndex; ii++) {
+            if ((i < startIndex + disconnectedPointCount || i > endIndex - disconnectedPointCount) && (ii < startIndex + disconnectedPointCount || ii > endIndex - disconnectedPointCount)) {
+                continue;
+            }
+
             const pointA = points[i];
             const pointB = points[ii];
             
