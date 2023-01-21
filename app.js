6f2048ddeedf50cc57388248490fe7f21d3618ff
Andrew Rittenhouse <andrewrittenhouse@gmail.com>
2023-01-20 19:24:57 -0500

    new release

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const publicPath = `/${process.env.ritualMadnessWebPublicPath ?? 'public'}`;
 const getFilename = (title) => title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
 
 const songs = [
+  ["Cyborg", [
+    "Samurai"
+  ]],
   ["Winter Warmer", [
     "Jolly",
     "Winter Night",
